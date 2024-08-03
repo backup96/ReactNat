a14191ad50bc8ee115379132080f327e7e609286
unknown <Aprendiz@BOGDFPCGMF1046.FormacionSena.RED>
2024-08-03 11:33:46 -0500

    Se configuro tailwind para el manejo de estilos en el proyecto de react

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,7 @@ import { Fragment } from "react";
 const RootLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }}/>
+      <Stack.Screen name='index' options={{headerShown: false}}/>
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { Slot } from 'expo-router'
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router'
 
-const RootLayout = () => {
-  return <Slot/>
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">
+        Aora adso
+      </Text>
+      <StatusBar style='auto'/>
+      <Link href={'/profile'} style={{ color: 'blue'}}>Go to profile</Link>
+    </View>
+  );
 }
-
-export default RootLayout
